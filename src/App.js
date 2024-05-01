4f2818151c08c6e56d6ae7442bb04107fb7e6905
Doug Issichopoulos <41125998+dougissi@users.noreply.github.com>
2024-05-01 16:55:31 -0700

    clean up state setters in handleInputText

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import AlertDialog from './components/AlertDialog';
 
 
 const numLetters = 5;
-const initialNumGuessesToShow = 6;
+const initialNumGuessesToShow = 2;
 
 function App() {
   const screenSize = useScreenSize();
@@ -25,16 +25,8 @@ function App() {
   // console.log(`${new Date()}`);
   // console.log(guessesData);
 
-  const evaluateGuess = (guess) => {
-    console.log(`TODO: evaluate ${guess}`);
-  };
-
   const handleInputText = (text) => {
     // console.log(`entered ${text}`);
-
-    const newGuessesData = [...guessesData];
-    const newNextLetterIndex = [...nextLetterIndex];
-    // console.log(newNextLetterIndex[1]);
 
     if (text === 'ENTER' && nextLetterIndex[1] === numLetters) {  // ENTER at end of word
       const guess = guessesData[nextLetterIndex[0]].join("").toLowerCase();
@@ -43,30 +35,35 @@ function App() {
       } else {
         evaluateGuess(guess);
 
-        newNextLetterIndex[0]++;
-        newNextLetterIndex[1] = 0;
-        if (newNextLetterIndex[0] === newGuessesData.length) {  // at end of all words
-          newGuessesData.push(Array(numLetters).fill(""));
+        const nextRowIndex = nextLetterIndex[0] + 1;
+        if (nextRowIndex === guessesData.length) {  // at end of all words
+          setGuessesData([...guessesData, Array(numLetters).fill("")]);  // add blank row
         }
+        setNextLetterIndex([nextRowIndex, 0]);
       }
     } else if ((text === 'BACKSPACE' || text === backspaceSymbol) && nextLetterIndex[1] > 0) {  // BACKSPACE with some letters
-      const updatedGuess = [...newGuessesData[nextLetterIndex[0]]];
+      const newGuessesData = [...guessesData];
+      const updatedGuess = [...guessesData[nextLetterIndex[0]]];
       updatedGuess[nextLetterIndex[1] - 1] = "";
       newGuessesData[nextLetterIndex[0]] = updatedGuess;
-      newNextLetterIndex[1]--;
-    } else if (isSingleEnglishLetter(text) && newNextLetterIndex[1] < numLetters) {
-      const newGuess = [...newGuessesData[nextLetterIndex[0]]];
-      newGuess[nextLetterIndex[1]] = text;
-      newGuessesData[nextLetterIndex[0]] = newGuess;
-      newNextLetterIndex[1]++;
+      setGuessesData(newGuessesData);
+      setNextLetterIndex([nextLetterIndex[0], nextLetterIndex[1] - 1]);
+    } else if (isSingleEnglishLetter(text) && nextLetterIndex[1] < numLetters) {  // Letter not at end
+      const newGuessesData = [...guessesData];
+      const updatedGuess = [...guessesData[nextLetterIndex[0]]];
+      updatedGuess[nextLetterIndex[1]] = text;
+      newGuessesData[nextLetterIndex[0]] = updatedGuess;
+      setGuessesData(newGuessesData);
+      setNextLetterIndex([nextLetterIndex[0], nextLetterIndex[1] + 1]);
     }
-
-    setGuessesData(newGuessesData);
-    setNextLetterIndex(newNextLetterIndex);
   };
 
   const handleDialogClose = () => {
     setDialogOpen(false);
+  };
+
+  const evaluateGuess = (guess) => {
+    console.log(`TODO: evaluate ${guess}`);
   };
 
   return (
--- todos.md
+++ todos.md
@@ -1,4 +1,5 @@
-* [ ] commonize screen size sharing
+* [x] commonize screen size sharing
 * [ ] dark mode within material ui?
 * [ ] figure out passing "theme" styling throughout
 * [ ] animate UI keyboard when typing on a physical keyboard
+* [ ] unit tests for utils.js
